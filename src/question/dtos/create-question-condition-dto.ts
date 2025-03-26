2f4ab76fc3a1137ad47e09871ce6a1c012834aad
VoVanBa <bavo4585@gmail.com>
2025-03-26 13:29:52 +0700

    fix condition

--- src/question/dtos/create-question-condition-dto.ts
+++ src/question/dtos/create-question-condition-dto.ts
@@ -4,6 +4,7 @@ import {
   IsEnum,
   ValidateNested,
   IsNotEmpty,
+  IsString,
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ConditionType } from '../entities/enums/ConditionType';
@@ -11,25 +12,47 @@ import { LogicalOperator } from '../entities/enums/LogicalOperator';
 import { ActionType } from '../entities/enums/ActionType';
 
 export class CreateQuestionLogicDto {
-  @IsInt()
-  @IsNotEmpty()
-  questionId: number;
+  @IsOptional()
+  @IsString()
+  sourceQuestionHeadline?: string;
+
+  @IsOptional()
+  @IsString()
+  sourceAnswerLabel?: string;
 
   @IsEnum(ConditionType)
   conditionType: ConditionType;
 
-  @IsNotEmpty()
-  conditionValue: any; // JSON chứa điều kiện (cần validate kỹ hơn trong service)
-
-  @IsEnum(LogicalOperator)
-  @IsOptional()
-  logicalOperator?: LogicalOperator;
-
   @IsEnum(ActionType)
-  @IsNotEmpty()
   actionType: ActionType;
 
-  @IsInt()
   @IsOptional()
-  jumpToQuestionId?: number;
+  @IsEnum(LogicalOperator)
+  logicalOperator?: LogicalOperator;
+
+  @IsOptional()
+  @IsString()
+  targetQuestionHeadline?: string;
+
+  // @IsInt()
+  // @IsNotEmpty()
+  // questionId: number;
+
+  // @IsEnum(ConditionType)
+  // conditionType: ConditionType;
+
+  // @IsNotEmpty()
+  // conditionValue: any; // JSON chứa điều kiện (cần validate kỹ hơn trong service)
+
+  // @IsEnum(LogicalOperator)
+  // @IsOptional()
+  // logicalOperator?: LogicalOperator;
+
+  // @IsEnum(ActionType)
+  // @IsNotEmpty()
+  // actionType: ActionType;
+
+  // @IsInt()
+  // @IsOptional()
+  // jumpToQuestionId?: number;
 }
